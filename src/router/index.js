e37c34eb9356f9bccd896dfe46452aec6588dc01
fengyongqiang <850513335@qq.com>
2018-05-31 18:09:28 +0800

    哈哈发发

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import HelloWorld from '../components/HelloWorld'
+import Login  from '../components/Login'
+
+
 
 Vue.use(Router)
 
@@ -10,6 +13,12 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/Login',
+      name: 'Login',
+      component: Login
     }
   ]
 })
+
